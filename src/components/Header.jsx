ef0a57900105c9250840b7c66a47cea448f5de94
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-08 16:59:42 +0100

    flashed out header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default function Header(data) {
-  return <div className="header-container">Header</div>;
+export default function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-fighter">
+        <div className="header-image-container">
+          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" />
+        </div>
+        <span>Computer</span>
+      </div>
+
+      <div className="header-fighter">
+        <div className="header-image-container">
+          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" />
+        </div>
+        <span>User</span>
+      </div>
+    </div>
+  );
 }
